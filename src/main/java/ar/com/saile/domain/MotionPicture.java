371e1381c18e1e825b39efa7bc8ae43c3cbd9f31
Elias Nicolas <sailenicolas@users.noreply.github.com>
2022-06-29 16:43:40 -0300

    added missing field

--- src/main/java/ar/com/saile/domain/MotionPicture.java
+++ src/main/java/ar/com/saile/domain/MotionPicture.java
@@ -24,6 +24,7 @@ import java.util.Collection;
 public class MotionPicture implements Serializable {
     static final String ID = "id";
 
+    @JsonView(Views.SearchMotionPicture.class)
     protected String image;
     @JsonView(Views.SearchMotionPicture.class)
     protected String title;
